2ad974fcfc6cba20908f3d699e282cedd12ffe01
Dave Nolan <dpn.nolan@gmail.com>
2020-03-15 13:20:17 -0400

    Updates

---
+++ precache-manifest.7d105b3a4c3a1e97d093b0b8ee0ba346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9c18da98659e196af00af4daa61bc2",
+    "url": "/Lottery/index.html"
+  },
+  {
+    "revision": "1822f94c049b2711c233",
+    "url": "/Lottery/static/css/main.202ed7f8.chunk.css"
+  },
+  {
+    "revision": "ebe79581d614f34f7482",
+    "url": "/Lottery/static/js/2.6839a593.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Lottery/static/js/2.6839a593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1822f94c049b2711c233",
+    "url": "/Lottery/static/js/main.a1544599.chunk.js"
+  },
+  {
+    "revision": "c12cfcd85064f746f1c5",
+    "url": "/Lottery/static/js/runtime-main.6946d687.js"
+  }
+]);
